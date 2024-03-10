a50e1c7e1a24b79667a5e2782b2e0a2a91f160d6
KIM MINJEONG <minjeong030420@gmail.com>
2024-03-11 01:46:32 +0900

    김민정 2022113227

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 name = input("이름을 입력하세요: ")
 student_number = input("학번을 입력하세요: ")
+department = input("학과를 입력하세요: ")
 
-print("\n >> 이름: " + name + ", 학번: " + student_number)
+print("\n >> 이름: " + name + ", 학번: " + student_number + ", 학과: " + department)
